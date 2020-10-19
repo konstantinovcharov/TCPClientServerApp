be7a642fa0867d5837ea2f342b33edbaee0e3445
konstantinovcharov <konstantinovcharov@abv.bg>
2020-10-19 20:43:21 +0200

    client and server

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace TCPClientServerApp
 {
@@ -6,7 +9,22 @@ namespace TCPClientServerApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var client = new TcpClient();
+            client.Connect(IPAddress.Loopback, 5000);
+
+            var stream = client.GetStream();
+
+            var data = Encoding.UTF8.GetBytes("Hello");
+            
+            stream.Write(data);
+
+            data = new byte[client.ReceiveBufferSize];
+
+            var cnt = stream.Read(data);
+
+            var msg = Encoding.UTF8.GetString(data, 0, cnt);
+
+            Console.WriteLine($"Message from the server: {msg}");
         }
     }
 }
